4a9281fcd9bee9a9b0542332962195454486301f
Kean H Lau <khklau@users.noreply.github.com>
2021-04-24 14:23:03 +1000

    Issue#2 - replacing the log_error function with a macro that makes the FFI and error handling involved with logging less verbose

--- src/apache2.rs
+++ src/apache2.rs
@@ -6,10 +6,6 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-use std::ffi::CString;
-use std::os::raw::c_char;
-use std::os::raw::c_int;
-
 #[macro_export]
 macro_rules! cstr {
     ($s:expr) => {
@@ -18,26 +14,25 @@ macro_rules! cstr {
     };
 }
 
-unsafe impl Send for module {}
-unsafe impl Sync for module {}
-
-pub fn log_error(
-    file: *const c_char,
-    line: u32,
-    level: u32,
-    status_code: i32,
-    server_info: *const server_rec,
-    msg: CString,
-) {
-    unsafe {
-        ap_log_error_(
-            file,
-            line as c_int,
-            APLOG_NO_MODULE as c_int,
-            level as c_int,
-            status_code,
-            server_info,
-            msg.as_ptr(),
-        );
+#[macro_export]
+macro_rules! log_error{
+    ($server_expr: expr, $msg_expr: expr, $log_failure_expr: expr) => {
+        unsafe {
+            crate::apache2::ap_log_error_(
+                cstr!(file!()),
+                line!() as std::os::raw::c_int,
+                crate::apache2::APLOG_NO_MODULE as c_int,
+                crate::apache2::APLOG_ERR as c_int,
+                -1,
+                $server_expr,
+                match std::ffi::CString::new($msg_expr) {
+                    Err(_) => $log_failure_expr,
+                    Ok(err_msg) => err_msg.as_ptr(),
+                },
+            );
+        }
     }
 }
+
+unsafe impl Send for module {}
+unsafe impl Sync for module {}
