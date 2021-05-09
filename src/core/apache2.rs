d5125e92da95234975d987031458b8951d751c09
Kean H Lau <khklau@users.noreply.github.com>
2021-05-09 13:33:50 +1000

    Issue #4 - initial implementation of MemoryPool and PoolBox

--- src/core/apache2.rs
+++ src/core/apache2.rs
@@ -36,3 +36,32 @@ macro_rules! try_log_else {
 
 unsafe impl Send for module {}
 unsafe impl Sync for module {}
+
+use std::alloc::{GlobalAlloc, Layout};
+use std::boxed::Box;
+use std::os::raw::c_ulong;
+use std::ptr;
+
+struct MemoryPool {
+    pool: *mut apr_pool_t,
+}
+
+unsafe impl GlobalAlloc for MemoryPool {
+
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        if self.pool != ptr::null_mut() {
+            let ptr = apr_palloc(self.pool, layout.size() as apr_size_t);
+            if ptr != ptr::null_mut() {
+                return memset(ptr, 0, layout.size() as c_ulong) as *mut u8;
+            }
+        }
+        return ptr::null_mut()
+    }
+
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
+        // freeing individual allocations is not required for memory pools
+        ()
+    }
+}
+
+type PoolBox<T> = Box<T, MemoryPool>;
