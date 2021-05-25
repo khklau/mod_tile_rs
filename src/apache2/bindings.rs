133a64a48db8d90754bdcb6e2e989c88f2d45b14
Kean H Lau <khklau@users.noreply.github.com>
2021-05-26 09:38:18 +1000

    Issue #2 - the try_log_else macro is now fully hygenic by using gensym to generate identifiers for temporary variables

--- src/apache2/bindings.rs
+++ src/apache2/bindings.rs
@@ -6,6 +6,9 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+#[macro_use]
+use gensym::gensym;
+
 #[macro_export]
 macro_rules! cstr {
     ($s:expr) => {
@@ -17,20 +20,31 @@ macro_rules! cstr {
 #[macro_export]
 macro_rules! try_log_else {
     (($level: expr, $server_expr: expr, $msg_expr: expr) {$log_failure_expr: expr}) => {
-        unsafe {
-            crate::apache2::bindings::ap_log_error_(
-                cstr!(file!()),
-                line!() as std::os::raw::c_int,
-                crate::apache2::bindings::APLOG_NO_MODULE as c_int,
-                $level as c_int,
-                -1,
-                $server_expr,
-                match std::ffi::CString::new($msg_expr) {
-                    Err(_) => $log_failure_expr,
-                    Ok(err_msg) => err_msg.as_ptr(),
-                },
-            );
-        }
+        gensym::gensym!{
+            _try_log_else!{ $level, $server_expr, $msg_expr, $log_failure_expr } }
+    };
+}
+
+macro_rules! _try_log_else { (
+    $err_sym: ident,
+    $level: expr,
+    $server_expr: expr,
+    $msg_expr: expr,
+    $log_failure_expr: expr) => {
+        match std::ffi::CString::new($msg_expr) {
+            Err(_) => $log_failure_expr,
+            Ok($err_sym) => unsafe {
+                crate::apache2::bindings::ap_log_error_(
+                    cstr!(file!()),
+                    line!() as std::os::raw::c_int,
+                    crate::apache2::bindings::APLOG_NO_MODULE as c_int,
+                    $level as c_int,
+                    -1,
+                    $server_expr,
+                    $err_sym.as_ptr(),
+                );
+            },
+        };
     }
 }
 
