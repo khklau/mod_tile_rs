7afee89245a584d3e25a24a91c2fea632798b5a1
Kean H Lau <khklau@users.noreply.github.com>
2021-06-29 15:45:33 +1000

    Issue #6 - parsing module status requests

--- src/slippy/request.rs
+++ src/slippy/request.rs
@@ -14,26 +14,50 @@ use crate::apache2::worker::WorkerContext;
 use std::boxed::Box;
 use std::convert::From;
 use std::error::Error;
-use std::ffi::CString;
+use std::ffi::CStr;
 use std::fmt;
 use std::io::Write;
 use std::os::raw::{c_char, c_int, c_void,};
 use std::option::Option;
 use std::ptr;
 use std::result::Result;
+use std::string::String;
+use std::str::Utf8Error;
 
 pub struct RequestContext<'r> {
     pub record: &'r mut request_rec,
     pub worker: &'r mut WorkerContext<'r>,
     pub connection: &'r mut ConnectionContext<'r>,
-    pub command: Option<Command>,
-    pub file_name: Option<CString>,
+    pub uri: &'r str,
+    pub request: Option<Request>,
 }
 
-pub enum Command {
+pub enum Request {
     ReportModStats,
     DescribeLayer,
-    ServeTile,
+    ServeTileV3(ServeTileRequestV3),
+    ServeTileV2(ServeTileRequestV2),
+}
+
+pub struct DescribeLayerRequest {
+    layer: i32,
+}
+
+pub struct ServeTileRequestV3 {
+    parameter: String,
+    x: i32,
+    y: i32,
+    z: i32,
+    extension: String,
+    option: Option<String>,
+}
+
+pub struct ServeTileRequestV2 {
+    x: i32,
+    y: i32,
+    z: i32,
+    extension: String,
+    option: Option<String>,
 }
 
 impl<'r> RequestContext<'r> {
@@ -55,22 +79,31 @@ impl<'r> RequestContext<'r> {
                 arg: "request_rec.connection".to_string(),
                 reason: "null pointer".to_string(),
             }));
+        } else if record.uri == ptr::null_mut() {
+            return Err(Box::new(InvalidArgError{
+                arg: "request_rec.uri".to_string(),
+                reason: "null pointer".to_string(),
+            }));
         }
         unsafe {
+            log!(APLOG_ERR, record.server, "RequestContext::find_or_create - start");
             let context = match Self::find(&mut *(record.pool)) {
                 Some(existing_context) => existing_context,
                 None => {
                     let server = &mut *(record.server);
                     let pool = &mut *(record.pool);
                     let connection = &mut *(record.connection);
-                    Self::create(record, pool, server, connection)?
+                    let uri = CStr::from_ptr(record.uri).to_str()?;
+                    Self::create(record, pool, server, connection, uri)?
                 },
             };
+            log!(APLOG_ERR, context.record.server, "RequestContext::find_or_create - finish");
             return Ok(context);
         }
     }
 
     fn find(request_pool: &'r mut apr_pool_t) -> Option<&'r mut Self> {
+        plog!(APLOG_ERR, request_pool, "RequestContext::find - start");
         let mut context_ptr: *mut RequestContext<'r> = ptr::null_mut();
         unsafe {
             let get_result = apr_pool_userdata_get(
@@ -80,8 +113,10 @@ impl<'r> RequestContext<'r> {
             );
             if get_result == (APR_SUCCESS as i32) {
                 let existing_context = &mut (*context_ptr);
+                plog!(APLOG_ERR, request_pool, "RequestContext::find success - finish");
                 return Some(existing_context);
             } else {
+                plog!(APLOG_ERR, request_pool, "RequestContext::find failed - finish");
                 return None;
             }
         }
@@ -92,10 +127,15 @@ impl<'r> RequestContext<'r> {
         record_pool: &'r mut apr_pool_t,
         server: &'r mut server_rec,
         connection: &'r mut conn_rec,
+        uri: &'r str,
     ) -> Result<&'r mut Self, Box<dyn Error>> {
+        log!(APLOG_ERR, server, "RequestContext::create - start");
         let pool_ptr = record_pool as *mut apr_pool_t;
         let new_context = alloc::<RequestContext<'r>>(record_pool)?;
         new_context.record = record;
+        new_context.worker = WorkerContext::find_or_create(server)?;
+        new_context.connection = ConnectionContext::find_or_create(connection)?;
+        new_context.uri = uri;
         unsafe {
             apr_pool_userdata_set(
                 new_context as *mut _ as *mut c_void,
@@ -104,91 +144,174 @@ impl<'r> RequestContext<'r> {
                 pool_ptr
             );
         }
-        new_context.worker = WorkerContext::find_or_create(server)?;
-        new_context.connection = ConnectionContext::find_or_create(connection)?;
+        log!(APLOG_ERR, new_context.worker.record, "RequestContext::create - finish");
         return Ok(new_context);
     }
 }
 
 #[no_mangle]
-pub unsafe extern fn drop_request_context(request_void: *mut c_void) -> apr_status_t {
-    if request_void == ptr::null_mut() {
+pub unsafe extern fn drop_request_context(context_void: *mut c_void) -> apr_status_t {
+    if context_void == ptr::null_mut() {
         return APR_BADARG as apr_status_t;
     }
-    let request_ptr = request_void as *mut RequestContext;
-    let request_ref = &mut *request_ptr;
-    drop(request_ref);
+    let context_ptr = context_void as *mut RequestContext;
+    log!(APLOG_ERR, (&mut *context_ptr).record.server, "drop_request_context - start");
+    let context_ref = &mut *context_ptr;
+    drop(context_ref);
+    log!(APLOG_ERR, (&mut *context_ptr).record.server, "drop_request_context - finish");
     return APR_SUCCESS as apr_status_t;
 }
 
 #[no_mangle]
-pub extern "C" fn translate(record_ptr: *mut request_rec) -> c_int {
+pub extern "C" fn parse(record_ptr: *mut request_rec) -> c_int {
     if record_ptr == ptr::null_mut() {
         return HTTP_INTERNAL_SERVER_ERROR as c_int;
     } else {
         unsafe {
-            let mut record = *record_ptr;
-            let context = match RequestContext::find_or_create(&mut record) {
+            let record = &mut *record_ptr;
+            log!(APLOG_ERR, record.server, "slippy::request::parse - start");
+            let context = match RequestContext::find_or_create(record) {
                 Ok(context) => context,
                 Err(_) => return HTTP_INTERNAL_SERVER_ERROR as c_int,
             };
-            match _translate(context) {
-                Ok(file_name) => {
-                    context.file_name = Some(file_name);
+            match _parse(context) {
+                Ok(request) => {
+                    context.request = Some(request);
+                    log!(APLOG_ERR, record.server, "slippy::request::parse - finish");
                     return OK as c_int;
                 },
                 Err(err) => match err {
-                    TranslateError::Param(err) => {
+                    ParseError::Param(err) => {
                         log!(
                             APLOG_NOTICE,
                             record.server,
                             format!("Parameter {} error: {}", err.param, err.reason)
                         );
                         return DECLINED as c_int;
-                    }
-                    TranslateError::Io(why) => {
+                    },
+                    ParseError::Io(why) => {
                         log!(APLOG_ERR, record.server, format!("IO error: {}", why));
                         return HTTP_INTERNAL_SERVER_ERROR as c_int;
-                    }
+                    },
+                    ParseError::Utf8(why) => {
+                        log!(APLOG_ERR, record.server, format!("UTF8 error: {}", why));
+                        return HTTP_INTERNAL_SERVER_ERROR as c_int;
+                    },
                 },
             }
         }
     }
 }
 
-fn _translate(context: &RequestContext) -> Result<CString, TranslateError> {
-    context.worker.trace_file.borrow_mut().write_all(b"slippy::request::translate - start\n")?;
-    context.worker.trace_file.borrow_mut().write_all(b"slippy::request::translate - finish\n")?;
-    Ok(CString::new("blah").unwrap())
+fn _parse(context: &RequestContext) -> Result<Request, ParseError> {
+    context.worker.trace_file.borrow_mut().write_all(b"slippy::request::_parse - start\n")?;
+    write!(context.worker.trace_file.borrow_mut(), "slippy::request::_parse - url.path={}\n", context.uri)?;
+
+    // try match stats request
+    let module_name = unsafe {
+        CStr::from_ptr(crate::TILE_MODULE.name).to_str()?
+    };
+    let stats_uri = format!("/{}", module_name);
+    if context.uri.eq(&stats_uri) {
+        context.worker.trace_file.borrow_mut().write_all(b"slippy::request::_parse ReportModStats - finish\n")?;
+        return Ok(Request::ReportModStats);
+    }
+
+    // try match ServeTileV3
+    match scan_fmt!(
+        context.uri,
+        "/{40[^/]}/{d}/{d}/{d}.{255[a-z]}/{10}",
+        String, i32, i32, i32, String, String
+    ) {
+        Ok((parameter, x, y, z, extension, option)) => {
+            context.worker.trace_file.borrow_mut().write_all(b"slippy::request::_parse ServeTileV3 - finish\n")?;
+            return Ok(Request::ServeTileV3(
+                ServeTileRequestV3 {
+                    parameter,
+                    x,
+                    y,
+                    z,
+                    extension,
+                    option: if option.is_empty() {
+                        None
+                    } else {
+                        Some(option)
+                    },
+                }
+            ))
+        },
+        Err(_) => ()
+    }
+
+    // try match ServeTileV2
+    match scan_fmt!(
+        context.uri,
+        "/{d}/{d}/{d}.{255[a-z]}/{10}",
+        i32, i32, i32, String, String
+    ) {
+        Ok((x, y, z, extension, option)) => {
+            context.worker.trace_file.borrow_mut().write_all(b"slippy::request::_parse ServeTileV2 - finish\n")?;
+            return Ok(Request::ServeTileV2(
+                ServeTileRequestV2 {
+                    x,
+                    y,
+                    z,
+                    extension,
+                    option: if option.is_empty() {
+                        None
+                    } else {
+                        Some(option)
+                    },
+                }
+            ))
+        },
+        Err(_) => ()
+    }
+    context.worker.trace_file.borrow_mut().write_all(b"slippy::request::_parse no matches - finish\n")?;
+    return Err(ParseError::Param(
+        InvalidParameterError {
+            param: "uri".to_string(),
+            reason: "Does not match any known request types".to_string(),
+        }
+    ));
 }
 
 #[derive(Debug)]
-enum TranslateError {
+enum ParseError {
     Param(InvalidParameterError),
     Io(std::io::Error),
+    Utf8(Utf8Error),
 }
 
-impl Error for TranslateError {
+impl Error for ParseError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         match self {
-            TranslateError::Param(err) => return Some(err),
-            TranslateError::Io(err) => return Some(err),
+            ParseError::Param(err) => return Some(err),
+            ParseError::Io(err) => return Some(err),
+            ParseError::Utf8(err) => return Some(err),
         }
     }
 }
 
-impl fmt::Display for TranslateError {
+impl fmt::Display for ParseError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            TranslateError::Param(err) => return write!(f, "{}", err),
-            TranslateError::Io(err) => return write!(f, "{}", err),
+            ParseError::Param(err) => return write!(f, "{}", err),
+            ParseError::Io(err) => return write!(f, "{}", err),
+            ParseError::Utf8(err) => return write!(f, "{}", err),
         }
     }
 }
 
-impl From<std::io::Error> for TranslateError {
+impl From<std::io::Error> for ParseError {
     fn from(error: std::io::Error) -> Self {
-        return TranslateError::Io(error);
+        return ParseError::Io(error);
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(error: Utf8Error) -> Self {
+        return ParseError::Utf8(error);
     }
 }
 
