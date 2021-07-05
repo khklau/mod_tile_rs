70800e65baaa3a542fffb811c8b999a6f622ffd2
Kean H Lau <khklau@users.noreply.github.com>
2021-07-05 14:01:07 +1000

    Issue #10 - replacing WorkerContext with VirtualHostContext
    Although a worker concept exists in Apache this is never exposed to modules. Instead a server_rec is meant to represent a virtual host.

--- src/storage/file_system.rs
+++ src/storage/file_system.rs
@@ -3,7 +3,7 @@
 use crate::apache2::bindings::{
     apr_pool_t, server_rec, APLOG_ERR,
 };
-use crate::apache2::worker::WorkerContext;
+use crate::apache2::virtual_host::VirtualHostContext;
 
 use std::io::Write;
 use std::ptr;
@@ -18,10 +18,10 @@ pub extern fn initialise(
         && server_info != ptr::null_mut() {
         unsafe {
             log!(APLOG_ERR, server_info, "storage::file_system::initialise - start");
-            let context = match WorkerContext::find_or_create(&mut *server_info) {
+            let context = match VirtualHostContext::find_or_create(&mut *server_info) {
                 Ok(context) => context,
                 Err(why) => {
-                    log!(APLOG_ERR, server_info, format!("Failed to create WorkerContext: {}", why));
+                    log!(APLOG_ERR, server_info, format!("Failed to create VirtualHostContext: {}", why));
                     return ();
                 }
             };
@@ -37,7 +37,7 @@ pub extern fn initialise(
 }
 
 fn _initialise(
-    context : &WorkerContext,
+    context : &VirtualHostContext,
 ) -> Result<(), std::io::Error> {
     context.trace_file.borrow_mut().write_all(b"storage::file_system::initialise - start\n")?;
     context.trace_file.borrow_mut().write_all(b"storage::file_system::initialise - finish\n")?;
