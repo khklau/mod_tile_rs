35e48df29b88289ec5c4200a9ee0a7f2a3cbcf3f
Kean H Lau <khklau@users.noreply.github.com>
2021-09-02 21:28:41 +1000

    Issue #12 - adding more unit test cases

--- src/tile/config.rs
+++ src/tile/config.rs
@@ -1,5 +1,7 @@
 use configparser::ini::Ini;
 
+use std::error::Error;
+use std::fmt;
 use std::path::Path;
 use std::result::Result;
 use std::string::String;
@@ -66,6 +68,14 @@ impl From<String> for ParseError {
     }
 }
 
+impl Error for ParseError {}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "TileConfig parsing failed: {}", self.reason)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -91,6 +101,58 @@ mod tests {
         let mut file_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         file_path.push("resources/test/tile/basic_invalid.conf");
         assert!(TileConfig::load(file_path.as_path()).is_ok(), "Invalid file was parsed");
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_config_name() -> Result<(), Box<dyn Error>> {
+        let mut ini = Ini::new();
+        ini.set("renderd", "socketname", Some(String::from("/var/run/renderd/renderd.sock")));
+        ini.set("MAPNIK", "font_dir", Some(String::from("/usr/share/fonts/")));
+        ini.set("foobar", "uri", Some(String::from("/foo/")));
+        let actual_config = _parse(&ini)?;
+        assert_eq!("foobar", actual_config.name, "Failed to parse config name from section");
+        assert_eq!("/var/run/renderd/renderd.sock", actual_config.ipc_uri, "Failed to parse socketname");
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_uri_with_trailing_slash() -> Result<(), Box<dyn Error>> {
+        let mut ini = Ini::new();
+        ini.set("basic", "uri", Some(String::from("/foo/")));
+        let actual_config = _parse(&ini)?;
+        assert_eq!("/foo", actual_config.base_url, "Failed to trim trailing slash from URI");
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_parameterize_style_as_bool() -> Result<(), Box<dyn Error>> {
+        let mut ini1 = Ini::new();
+        ini1.set("basic", "parameterize_style", Some(String::from("TRUE")));
+        let actual_config1 = _parse(&ini1)?;
+        assert!(actual_config1.parameters_allowed, "Failed to parse parameterize_style");
+
+        let mut ini2 = Ini::new();
+        ini2.set("basic", "parameterize_style", Some(String::from("false")));
+        let actual_config2 = _parse(&ini2)?;
+        assert!(!actual_config2.parameters_allowed, "Failed to parse parameterize_style");
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_invalid_parameterize_style() -> Result<(), Box<dyn Error>> {
+        let mut ini = Ini::new();
+        ini.set("basic", "parameterize_style", Some(String::from("yes")));
+        assert!(_parse(&ini).is_err(), "Invalid parameterize_size value was not rejected");
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_uppercase_section_and_key() -> Result<(), Box<dyn Error>> {
+        let mut ini = Ini::new();
+        ini.set("BASIC", "TILE_DIR", Some(String::from("/var/cache/renderd/")));
+        let actual_config = _parse(&ini)?;
+        assert_eq!("/var/cache/renderd/", actual_config.store_uri, "Failed to parse upper case tile_dir");
         Ok(())
     }
 }
