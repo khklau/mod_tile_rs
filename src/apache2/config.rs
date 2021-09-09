c900ad8fcab0cae99971669a450cc5a09d867ac9
Kean H Lau <khklau@users.noreply.github.com>
2021-09-09 15:16:39 +1000

    Issue #12 - refactoring to streamline error handling

--- src/apache2/config.rs
+++ src/apache2/config.rs
@@ -1,13 +1,16 @@
 use crate::apache2::bindings::{
-    cmd_parms,
+    cmd_parms, server_rec,
 };
 use crate::apache2::virtual_host::VirtualHostContext;
 use crate::tile::config::load;
 
+use std::boxed::Box;
+use std::error::Error;
 use std::ffi::CStr;
 use std::os::raw::{ c_char, c_void, };
 use std::path::Path;
 use std::ptr;
+use std::result::Result;
 
 
 #[no_mangle]
@@ -23,20 +26,21 @@ pub extern "C" fn load_tile_config(
     if cmd_context.server == ptr::null_mut() {
         return cstr!("Null server_rec");
     }
-    let path_str = match unsafe { CStr::from_ptr(value).to_str() } {
-        Ok(path) => path,
-        Err(_) => return cstr!("LoadTileConfig value contains invalid UTF-8"),
-    };
-    let file_path = Path::new(path_str);
     let server = unsafe { &mut *(cmd_context.server) };
-    let host_context = match VirtualHostContext::find_or_create(server) {
-        Ok(context) => context,
-        Err(_) => return cstr!("Invalid server_rec"),
+    let path_str = unsafe { CStr::from_ptr(value).to_str().unwrap() };
+    match _load_tile_config(server, path_str) {
+        Ok(_) => return ptr::null(),
+        Err(_) => return cstr!("Failed to load tile config file"),
     };
-    let tile_config = match load(file_path) {
-        Ok(tile_config) => tile_config,
-        Err(_) => return cstr!("Failed to parse tile config file"),
-    };
+}
+
+fn _load_tile_config(
+    server: &mut server_rec,
+    path_str: &str,
+) -> Result<(), Box<dyn Error>> {
+    let file_path = Path::new(path_str);
+    let tile_config = load(file_path)?;
+    let host_context = VirtualHostContext::find_or_create(server)?;
     host_context.tile_config = tile_config;
-    return ptr::null();
+    return Ok(());
 }
