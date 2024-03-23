f6ffa21e8cc8b29c6f83c918b05f9caf6be4c76f
Kean H Lau <khklau@users.noreply.github.com>
2024-03-24 01:00:33 +1100

    Issue #32 - adding initialisation scripts for osm-postgis container

---
+++ devops/container/osm-postgis/createdb_osm.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Creating user '${POSTGRES_USER}'"
+adduser --system --group ${POSTGRES_USER}
+createuser ${POSTGRES_USER}
+
+echo "Creating database '${POSTGRES_DB}'"
+dropdb ${POSTGRES_DB}
+createdb -E UTF8 -O ${POSTGRES_USER} ${POSTGRES_DB}
