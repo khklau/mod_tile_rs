103e00b101a2602b02a85d1e4a087aa5b16cd13d
Kean H Lau <khklau@users.noreply.github.com>
2024-05-18 14:52:51 +1000

    Issuer #32 - httpd virtualhost and site config fixes

--- devops/container/docker_run.sh
+++ devops/container/docker_run.sh
@@ -4,6 +4,17 @@ if [ -n "$(docker container ps --all | awk '{print $2}' | grep '^aus-osm-postgis
 	docker container stop aus-osm-postgis
 	docker container rm aus-osm-postgis
 fi
-
 docker container run --name aus-osm-postgis --network=osm-local --detach -p 5432:5432 aus-osm-postgis:13-3.4
+
+if [ -n "$(docker container ps --all | awk '{print $2}' | grep '^legacy-tileserver:2.4.57-0.5-2')" ]; then
+	docker container stop legacy-tileserver
+	docker container rm legacy-tileserver
+fi
+docker container run --name legacy-tileserver --network=osm-local -p 8080:8080 legacy-tileserver:2.4.57-0.5-2
+
+docker container stop legacy-tileserver
+docker container rm legacy-tileserver
+
+docker container stop aus-osm-postgis
+docker container rm aus-osm-postgis
 
