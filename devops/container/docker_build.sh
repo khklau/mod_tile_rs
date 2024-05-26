e30445b34ad55ccf8445c05c3720422f6fd46bdc
Kean H Lau <khklau@users.noreply.github.com>
2024-05-26 12:03:03 +1000

    Issuer #32 - adding volume for the Postgres run directory so other contains can access the Unix domain socket
    Despite the apparent configurability during stylesheet generation, renderd actually requires the DB name to be gis

--- devops/container/docker_build.sh
+++ devops/container/docker_build.sh
@@ -11,6 +11,10 @@ if [ -z "$(docker volume ls | grep shared-legacy-renderer)" ]; then
     docker volume create --name shared-legacy-renderer
 fi
 
+if [ -z "$(docker volume ls | grep postgis-run)" ]; then
+    docker volume create --name postgis-run
+fi
+
 if [ -z "$(docker image ls | grep '^osm-postgis' | grep '13-3.4')" ]; then
     docker build --tag osm-postgis:13-3.4 ${CONTAINER_DIR}/osm-postgis
 fi
@@ -29,10 +33,11 @@ if [ -z "$(docker image ls | grep '^aus-osm-postgis' | grep '13-3.4')" ]; then
     docker container rm osm-data-ingestion
     docker container stop osm-postgis
     docker container rm osm-postgis
-    docker container run --name osm-postgis --rm --network=osm-local --detach -p 5432:5432 osm-postgis:13-3.4 \
+    docker container run --name osm-postgis --rm --volume postgis-run:/run/postgresql --network=osm-local --detach -p 5432:5432 osm-postgis:13-3.4 \
         && sleep 10 \
         && docker container run --name osm-data-ingestion --rm --network=osm-local osm-data-ingestion:1.4.1 \
         && docker container commit $(docker container ps --all | grep osm-postgis:13-3.4 | awk '{print $1}') aus-osm-postgis:13-3.4
+    docker container stop osm-postgis
 fi
 
 if [ -n "$(docker container ps --all | awk '{print $2}' | grep '^aus-osm-postgis:13-3.4')" ]; then
@@ -44,11 +49,12 @@ if [ -n "$(docker container ps --all | awk '{print $2}' | grep '^stylesheet-gene
     docker container rm stylesheet-generation
 fi
 if [ ! -e ${CONTAINER_DIR}/legacy-renderer/style.xml ]; then
-    docker container run --name aus-osm-postgis --rm --network=osm-local --detach -p 5432:5432 aus-osm-postgis:13-3.4
+    docker container run --name aus-osm-postgis --rm --volume postgis-run:/run/postgresql --network=osm-local --detach -p 5432:5432 aus-osm-postgis:13-3.4
     sleep 10
-    docker container run --name stylesheet-generation --rm --network=osm-local stylesheet-generation:5.3.1 \
+    docker container run --name stylesheet-generation --network=osm-local stylesheet-generation:5.3.1 \
         && docker cp stylesheet-generation:/home/osm/osm-carto/style.xml ${CONTAINER_DIR}/legacy-renderer/style.xml \
         && docker cp stylesheet-generation:/home/osm/osm-carto/style.xml ${CONTAINER_DIR}/next-renderer/style.xml
+    docker container rm stylesheet-generation
 fi
 
 # Since legacy-tileserver needs files from multiple directories the build context directory needs to be ${CONTAINER_DIR}
--- devops/container/docker_run.sh
+++ devops/container/docker_run.sh
@@ -2,31 +2,30 @@
 
 if [ -n "$(docker container ps --all | awk '{print $2}' | grep '^aus-osm-postgis:13-3.4')" ]; then
     docker container stop aus-osm-postgis
-    docker container rm aus-osm-postgis
 fi
 docker container run \
     --name aus-osm-postgis \
     --detach \
     --rm \
+    --volume postgis-run:/run/postgresql \
     --network=osm-local \
     --publish 5432:5432 \
     aus-osm-postgis:13-3.4
 
 if [ -n "$(docker container ps --all | awk '{print $2}' | grep '^legacy-renderer:0.5-2')" ]; then
     docker container stop legacy-renderer
-    docker container rm legacy-renderer
 fi
 docker container run \
     --name legacy-renderer \
     --detach \
     --rm \
+    --volume postgis-run:/run/postgresql \
     --volume shared-legacy-renderer:/shared \
     --network=osm-local \
     legacy-renderer:0.5-2
 
 if [ -n "$(docker container ps --all | awk '{print $2}' | grep '^legacy-tileserver:2.4.57-0.5-2')" ]; then
     docker container stop legacy-tileserver
-    docker container rm legacy-tileserver
 fi
 docker container run \
     --name legacy-tileserver \
